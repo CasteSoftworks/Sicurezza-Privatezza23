c0f53eca4ed610ba1c45997d49ad077a983fdd58
Giacomo Castellana <giacomo.castellana01@gmail.com>
2023-12-25 11:39:21 +0100

    Add files via upload

--- PwnCollege/pwn_Assembly/asmScript06.s
+++ PwnCollege/pwn_Assembly/asmScript06.s
@@ -8,4 +8,12 @@
 
 .section                .text
 _start:
-			
+	#sul registo a 8b di rax metto il valore presente sul registro a 8b di rdi 
+	#--> contenete 0xff
+	mov al, dil
+	#sul registro a 16b di rbx metto il valore presente sul registro a 16b di rsi 
+	#--> contenente 0xfff
+	mov bx, si
+
+#codice in hex solo istruzioni
+# - 4088F86689F3
---
+++ PwnCollege/pwn_Assembly/asmScript07.s
@@ -0,0 +1,6 @@
+mov rax, rdi
+shl rax, 24
+shr rax, 56
+
+#in hex
+# - 4889F848C1E01848C1E838
