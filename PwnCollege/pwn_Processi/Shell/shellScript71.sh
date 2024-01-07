db2910cbebfba5bd464a51c91e8638b36816ddc5
Giacomo Castellana <giacomo.castellana01@gmail.com>
2024-01-07 14:25:26 +0100

    unione 68 e 70 (argv e env assieme)

---
+++ PwnCollege/pwn_Processi/Shell/shellScript71.sh
@@ -0,0 +1,7 @@
+null_args=()
+for ((i=1; i<=94; i++)); do
+    null_args+=("")
+done
+
+arguments=("${null_args[@]}" "eqpuktmlvi")
+env -i env 43=tmjavkjciq /challenge/embryoio_level71 "${arguments[@]}"
