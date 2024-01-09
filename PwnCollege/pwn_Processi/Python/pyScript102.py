e15302486b5291a10e98fe5f9b320533eaa9a13d
Giacomo Castellana <giacomo.castellana01@gmail.com>
2024-01-09 19:30:01 +0100

    prima ln -s poi aggiornare path

---
+++ PwnCollege/pwn_Processi/Python/pyScript102.py
@@ -0,0 +1,7 @@
+#prima metti ln -s /challenge/embryoio_level102 TUOFILE
+#poi export PATH=$PATH:/home/hacker/Desktop/python
+import subprocess
+
+executable_path = "wjslpk"
+
+subprocess.run(executable_path)
