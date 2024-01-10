847ba7115223ad1897b236413dacf7b1aebea515
Giacomo Castellana <giacomo.castellana01@gmail.com>
2024-01-10 11:20:21 +0100

    passare in argomento un valore

---
+++ PwnCollege/pwn_Processi/C/cScript115.c
@@ -0,0 +1,22 @@
+//aa
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void pwncollege(){}
+
+int main(){
+
+	 pid_t pid = fork();
+	 chdir("..");
+ 	 if (pid == 0) {
+	 	execl("/challenge/embryoio_level115","ascnxj", (char *)NULL);
+	 } else {
+ 	        // Parent process
+ 	        // Optionally, you can wait for the child process to finish
+ 	        wait();
+		printf("lol papa'");
+ 	 }
+
+ 	 return 0;
+}
